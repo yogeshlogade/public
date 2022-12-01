22ca8cab45dc51a9fe72c7c392af75787daea1d7
Shubham Vairagade <shubhamvairagade91@gmail.com>
2022-12-01 23:49:32 +0530

    my work is done please check it

--- flip.js
+++ flip.js
@@ -1,26 +1,31 @@
 
-//checking prim number
+//checking prime number
 
 let num=21;
-let count =0
+let count =0;
 for(let i=1;i<=num;i++){
     if(num%i==0){
-        count++
+        count++;
 
     }
-}if(count=undefined){
-    console.log("Prim")
-}else{
-    console.log("Not Prim")
+}
+if(count==2){
+    console.log("Prime");
+}
+else{
+    console.log("Not Prime");
 }
 
 //problem2 check palindrome
- let string="madam"
-let bag =""
-for(i=0;i<string.length;i++)
+let string="madam";
+let bag ="";
+for(let i=string.length-1; i>=0 ;i--)
 {
- bag+=string[i]+" "
+  bag += string[i];
            }
 if(bag==string){
-console.log("it is palindrome")}
-else{console.log("it is not palindrome")
+ console.log("it is palindrome");
+}
+else{
+ console.log("it is not palindrome");
+}
